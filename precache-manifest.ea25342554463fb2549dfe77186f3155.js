64c49304884b0dbe54c18eada1ce1d074f2d84cc
rajju <vajrachara@yahoo.com>
2019-02-22 08:45:12 +0000

    Updates

---
+++ precache-manifest.ea25342554463fb2549dfe77186f3155.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "322b1500578f92720555",
+    "url": "/react-basic-weather-app/static/js/runtime~main.322b1500.js"
+  },
+  {
+    "revision": "a667115d4fdff5367f24",
+    "url": "/react-basic-weather-app/static/js/main.a667115d.chunk.js"
+  },
+  {
+    "revision": "ac1fb793f5150fd70578",
+    "url": "/react-basic-weather-app/static/js/2.ac1fb793.chunk.js"
+  },
+  {
+    "revision": "a667115d4fdff5367f24",
+    "url": "/react-basic-weather-app/static/css/main.8336af41.chunk.css"
+  },
+  {
+    "revision": "7b1265f96acfa4c3219f009e6a4448f2",
+    "url": "/react-basic-weather-app/index.html"
+  }
+];
